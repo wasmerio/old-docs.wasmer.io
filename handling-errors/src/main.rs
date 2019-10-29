75c4a9ead46edc66b48216000c6684ebcdae0939
Aaron Turner <aaron@aaronthedev.com>
2019-10-29 13:36:29 -0700

    Got the error example working

---
+++ handling-errors/src/main.rs
@@ -0,0 +1,50 @@
+// Import the Filesystem so we can read our .wasm file
+use std::io::prelude::*;
+use std::fs::File;
+
+// Import the wasmer runtime so we can use it
+use wasmer_runtime::{
+    instantiate,
+    Func,
+    imports,
+    error,
+};
+
+// Our entry point to our application
+fn main() -> error::Result<()> {
+
+    // Let's read in our .wasm file as bytes
+
+    // Let's open the file. 
+    // The file path may be different depending where you run `cargo run`, and where you place the file.
+    let mut file = File::open("./example-rust-wasm-crate/throw-wasm-error/pkg/throw_wasm_error_bg.wasm").expect("Incorrect file path to wasm module.");
+
+    // Let's read the file into a Vec
+    let mut wasm_vec = Vec::new();
+    file.read_to_end(&mut wasm_vec).expect("Error reading the wasm file");
+
+    // Let's get our byte slice ( [u8] ) from ouw wasm_vec.
+    let wasm_bytes = wasm_vec.as_slice();
+
+    // Now that we have the wasm file as bytes, let's run it with the wasmer runtime
+
+    // Our import object, that allows exposing functions to our wasm module.
+    // We're not importing anything, so make an empty import object.
+    let import_object = imports!{};
+
+    // Let's create an instance of wasm module running in the wasmer-runtime
+    let instance = instantiate(wasm_bytes, &import_object)?;
+
+    // Let's call the exported "throw_error" function ont the wasm module.
+    let throw_error_func: Func<(), ()> = instance
+        .func("throw_error")
+        .expect("throw_error");
+
+    throw_error_func.call().unwrap();
+
+    // Log a success message.
+    println!("Success!");
+
+    // Return OK since everything executed successfully!
+    Ok(())
+}
