4f1a3db8fb2df4473be04bed4b2c21f0b5c9f981
Aaron Turner <aaron@aaronthedev.com>
2019-11-18 12:52:26 -0800

    Got the char array buffer working!

--- c/passing-data/example-wasienv-wasm/strings-wasm-is-cool/strings-wasm-is-cool.c
+++ c/passing-data/example-wasienv-wasm/strings-wasm-is-cool/strings-wasm-is-cool.c
@@ -1,7 +1,14 @@
 
+// Define a fixed size bbyte array
+char byteBufferPointer[1000] = {0};
 
+char* getBufferPointer() {
+  return byteBufferPointer;
+}
 
 // Function that will add the string "Wasm is cool" to the pointer for the original string
-int addWasmIsCool(int wasmIsCoolPointer) {
-  
+char addWasmIsCool() {
+  // Get the current string in the buffer
+  byteBufferPointer[3] = 24;
+  return byteBufferPointer[0];
 }
