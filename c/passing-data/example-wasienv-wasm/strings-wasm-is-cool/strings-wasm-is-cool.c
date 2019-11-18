5b0986fe33fbeb6a3e55e6ba392688a97714b2b2
Aaron Turner <aaron@aaronthedev.com>
2019-11-18 13:08:49 -0800

    Got the C Example finished

--- c/passing-data/example-wasienv-wasm/strings-wasm-is-cool/strings-wasm-is-cool.c
+++ c/passing-data/example-wasienv-wasm/strings-wasm-is-cool/strings-wasm-is-cool.c
@@ -1,14 +1,19 @@
-
 // Define a fixed size bbyte array
-char byteBufferPointer[1000] = {0};
+char byteBuffer[1000] = {0};
 
 char* getBufferPointer() {
-  return byteBufferPointer;
+  return byteBuffer;
 }
 
 // Function that will add the string "Wasm is cool" to the pointer for the original string
-char addWasmIsCool() {
-  // Get the current string in the buffer
-  byteBufferPointer[3] = 24;
-  return byteBufferPointer[0];
+int addWasmIsCool(int bufferIndex) {
+  char* wasmIsCool = " Wasm is cool!";
+  int wasmIsCoolLength = sizeof(*wasmIsCool) / sizeof(wasmIsCool[0]);
+
+  for (int i = 0; i < wasmIsCoolLength; i++) {
+    byteBuffer[bufferIndex + i] = wasmIsCool[i];
+  }
+
+  // Return the new length of the string
+  return bufferIndex + wasmIsCoolLength;
 }
