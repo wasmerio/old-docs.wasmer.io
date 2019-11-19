6a66ea61961dc70a4704621a6657fb084d0554dd
Mark McCaskey <mark@wasmer.io>
2019-11-19 12:01:19 -0800

    Get memory access working; add import object abstraction

--- c/passing-data/passing-data.c
+++ c/passing-data/passing-data.c
@@ -20,12 +20,12 @@ wasmer_memory_t *create_wasmer_memory() {
   // .has_some represents wether or not the memory has a maximum size
   // .some is the value of the maxiumum size
   wasmer_limit_option_t max = { .has_some = true,
-    .some = 256 };
+                                .some = 256 };
   // Create our memory descriptor, to set our minimum and maximum memory size
   // .min is the minimum size of the memory
   // .max is the maximuum size of the memory
   wasmer_limits_t descriptor = { .min = 256,
-    .max = max };
+                                 .max = max };
 
   // Create our memory instance, using our memory and descriptor,
   wasmer_result_t memory_result = wasmer_memory_new(&memory, descriptor);
@@ -38,10 +38,37 @@ wasmer_memory_t *create_wasmer_memory() {
   return memory;
 }
 
-// Function to create our Wasmer Instance
-// And return it's memory
-wasmer_instance_t *create_wasmer_instance(wasmer_memory_t *memory) {
+wasmer_module_t *create_wasmer_module() {
+  // Read the wasm file bytes
+  // TODO: Check if file is NULL
+  FILE *file = fopen("example-wasienv-wasm/strings-wasm-is-cool/strings-wasm-is-cool.wasm", "r");
+  fseek(file, 0, SEEK_END);
+  long len = ftell(file);
+  uint8_t *bytes = malloc(len);
+  fseek(file, 0, SEEK_SET);
+  fread(bytes, 1, len, file);
+  fclose(file);
 
+  wasmer_module_t *module = NULL;
+  wasmer_result_t compile_result = wasmer_compile(&module, bytes, len);
+
+  // Print our the result of our compilation,
+  // Ensure the compilation was successful.
+  printf("Compile result:  %d\n", compile_result);
+  if (compile_result != WASMER_OK)
+  {
+    print_wasmer_error();
+  }
+  // Assert the wasm instantion completed
+  assert(compile_result == WASMER_OK);
+
+
+  free(bytes);
+
+  return module;
+}
+
+wasmer_import_object_t *create_import_object(wasmer_memory_t *memory) {
   // Create module name for our imports
 
   // Create a UTF-8 string as bytes for our module name. 
@@ -56,7 +83,7 @@ wasmer_instance_t *create_wasmer_instance(wasmer_memory_t *memory) {
   // And, place the string into the wasmer_byte_array type so it can be used by our guest wasm instance.
   const char *import_memory_name = "memory";
   wasmer_byte_array import_memory_name_bytes = { .bytes = (const uint8_t *) import_memory_name,
-    .bytes_len = strlen(import_memory_name) };
+                                                 .bytes_len = strlen(import_memory_name) };
 
   // Create our memory import object, from our passed memory,
   // that will be used as shared wasm memory between the host (this application),
@@ -65,45 +92,61 @@ wasmer_instance_t *create_wasmer_instance(wasmer_memory_t *memory) {
   // The .import_name is the key of the module that is within the importObject
   // The .tag is the type of import being added to the import object
   wasmer_import_t memory_import = { .module_name = module_name_bytes,
-    .import_name = import_memory_name_bytes,
-    .tag = WASM_MEMORY };
+                                    .import_name = import_memory_name_bytes,
+                                    .tag = WASM_MEMORY };
 
   // Set the memory to our import object
   memory_import.value.memory = memory;
+  // Define a global import
+  const char *import_global_name = "__memory_base";
+  wasmer_byte_array import_global_name_bytes = { .bytes = (const uint8_t *) import_global_name,
+                                                 .bytes_len = strlen(import_global_name) };
+  wasmer_import_t global_import = { .module_name = module_name_bytes,
+                                    .import_name = import_global_name_bytes,
+                                    .tag = WASM_GLOBAL };
+
+  wasmer_value_t val = { .tag = WASM_I32,
+                           .value.I32 = 1024 };
+  wasmer_global_t *global = wasmer_global_new(val, false);
+  global_import.value.global = global;
 
   // Define an array containing our imports
-  wasmer_import_t imports[] = {memory_import};
+  wasmer_import_t imports[] = {global_import, memory_import};
 
-  // Read the wasm file bytes
-  // TODO: Check if file is NULL
-  FILE *file = fopen("example-wasienv-wasm/strings-wasm-is-cool/strings-wasm-is-cool.wasm", "r");
-  fseek(file, 0, SEEK_END);
-  long len = ftell(file);
-  uint8_t *bytes = malloc(len);
-  fseek(file, 0, SEEK_SET);
-  fread(bytes, 1, len, file);
-  fclose(file);
+  
+  wasmer_import_object_t *import_object = wasmer_import_object_new();
+  wasmer_result_t extend_result = wasmer_import_object_extend(import_object, imports, 2);
 
-  // Instantiate a WebAssembly Instance from wasm bytes and imports
-  wasmer_instance_t *instance = NULL;
-  wasmer_result_t compile_result = wasmer_instantiate(
-      &instance, // Our reference to our wasm instance 
-      bytes, // The bytes of the WebAssembly modules
-      len, // The length of the bytes of the WebAssembly module
-      imports, // The Imports array the will be used as our importObject
-      1 // The number of imports in the imports array
-      );
-
-  // Print our the result of our compilation,
-  // Ensure the compilation was successful.
-  printf("Compile result:  %d\n", compile_result);
-  if (compile_result != WASMER_OK)
+  if (extend_result != WASMER_OK)
   {
     print_wasmer_error();
   }
 
   // Assert the wasm instantion completed
-  assert(compile_result == WASMER_OK);
+  assert(extend_result == WASMER_OK);
+
+  return import_object;
+}
+
+// Function to create our Wasmer Instance
+// And return it's memory
+wasmer_instance_t *create_wasmer_instance(wasmer_module_t *module, wasmer_import_object_t *import_object) {
+  // Instantiate a WebAssembly Instance from a module and import object
+  wasmer_instance_t *instance = NULL;
+  wasmer_result_t instantiate_result = wasmer_module_import_instantiate(&instance,
+                                                                        module,
+                                                                        import_object);
+
+  // Print our the result of our compilation,
+  // Ensure the compilation was successful.
+  printf("Instantiation result:  %d\n", instantiate_result);
+  if (instantiate_result != WASMER_OK)
+  {
+    print_wasmer_error();
+  }
+
+  // Assert the wasm instantion completed
+  assert(instantiate_result == WASMER_OK);
 
   return instance;
 }
@@ -132,13 +175,21 @@ int call_wasm_function_and_return_i32(wasmer_instance_t *instance, char* functio
   return response_value;
 }
 
+uint8_t *get_pointer_to_memory(wasmer_instance_t *instance) {
+  const wasmer_instance_context_t *ctx = wasmer_instance_context_get(instance);
+  const wasmer_memory_t *memory = wasmer_instance_context_memory(ctx, 0);
+
+  return wasmer_memory_data(memory);
+}
+
 int main() {
 
   // Initialize our Wasmer Memory and Instance
   wasmer_memory_t *memory = create_wasmer_memory();
-  uint8_t *memoryData = wasmer_memory_data(memory);
   uint32_t memoryLength = wasmer_memory_data_length(memory);
-  wasmer_instance_t *instance = create_wasmer_instance(memory);
+  wasmer_module_t *module = create_wasmer_module();
+  wasmer_import_object_t *import_object = create_import_object(memory);
+  wasmer_instance_t *instance = create_wasmer_instance(module, import_object);
 
   // Let's get the pointer to the buffer exposed by our Guest Wasm Module
   wasmer_value_t getBufferPointerParams[] = { 0 };
@@ -148,6 +199,8 @@ int main() {
 
   // Write our initial string into the buffer
   // Get a pointer to the memory Data
+  uint8_t *memoryData = get_pointer_to_memory(instance);
+  printf("memory pointer %p", memoryData);
   char *memoryBytes = memoryData;
   if (!memoryBytes) {
     // TODO: Handle the Null pointer
@@ -160,6 +213,7 @@ int main() {
   for(int i = -10; i < 10; i++) {
     printf("Surrounding Wasm value at buffer pointer, i: %d, *(memoryBytes + bufferPointer): %d\n", i, *(memoryBytes + bufferPointer + i));
   }
+
 
   // Write the string bytes to memory
   char originalString[13] = "Hello there,";
@@ -178,6 +232,7 @@ int main() {
   // Fetch out the new string
   char newString[100];
 
+  memoryData = get_pointer_to_memory(instance);
   for (int i = 0; i < newStringLength; i++) {
     char charInBuffer = *(memoryBytes + bufferPointer + i);
     printf("Char: %c\n", charInBuffer);
