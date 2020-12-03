81d2a5a661ebf4bd201795dbcb81ef02d4cb46a3
jubianchi <julien@wasmer.io>
2020-12-03 18:14:26 +0100

    remove old examples that have been ported

--- integrations/c/examples/handling-errors/build.sh
+++
@@ -1,2 +0,0 @@
-gcc handling-errors.c -I${WASMER_C_API}/include -L${WASMER_C_API}/lib -lwasmer -o handling-errors -rpath ${WASMER_C_API}/lib
-# Note: add `-rpath ${WASMER_C_API}/lib` if you are in macOS
